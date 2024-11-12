2c39bf03f9342a9952e7d46cafcfd9e1da9ddea8
SEUNGTO <tmdduq9008@gmail.com>
2024-11-12 12:45:11 +0900

    업데이트 중

--- get_detail_account.py
+++ get_detail_account.py
@@ -8,6 +8,10 @@ import os
 
 
 def dart_codeListing():
+    """
+    매일 돌아가면서 DART에서 코드를 리스팅할 함수
+    """
+
     url = 'https://opendart.fss.or.kr/api/corpCode.xml'
     params = {'crtfc_key': os.environ.get('DART_API_KEY')}
 
@@ -29,15 +33,24 @@ def dart_codeListing():
 
         corp_list.append([corp_code, corp_name, stock_code])
 
-    return pd.DataFrame(corp_list, columns=['고유번호', '정식명칭', '종목코드'])
-
-
-if __name__ == '__main__' : 
-
+    dart_code_list = pd.DataFrame(corp_list, columns=['고유번호', '정식명칭', '종목코드'])
+    dart_code_list = dart_code_list.loc[dart_code_list['종목코드'] != " ", :]
+    
     if not os.path.exists('data') :
         os.makedirs('data')
 
-    if not os.path.exists('corpCode') :
-        dart_code_list = dart_codeListing()
-        dart_code_list = dart_code_list.loc[dart_code_list['종목코드'] != " ", :]
-        dart_code_list.reset_index().to_json('data/dart_code_list.json')
+    return dart_code_list.reset_index(drop=True).to_json('data/dart_code_list.json')
+
+
+def get_dart_data() :
+    url = 'https://raw.githubusercontent.com/SEUNGTO/ETFACE_DATA/refs/heads/main/data/dart_code_list.json'
+    dart_code_list = requests.get(url).json()
+    dart_code_list = pd.DataFrame(dart_code_list)
+
+    print(dart_code_list)
+    pdb.set_trace()
+
+if __name__ == '__main__' : 
+
+    dart_codeListing()
+    # get_dart_data()
