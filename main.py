442b4ea6942e5f4711f75f75053302596978a934
SEUNGTO <68551302+SEUNGTO@users.noreply.github.com>
2025-02-12 09:59:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,25 +13,25 @@ if __name__ == '__main__' :
     # DB 엔진 생성
     engine = create_db_engine()
 
-    # # [작업1] 기본 정보 업데이트
-    # print('[작업1] 기본 정보 업데이트')
-    # update_basic_information(engine)
+    # [작업1] 기본 정보 업데이트
+    print('[작업1] 기본 정보 업데이트')
+    update_basic_information(engine)
 
-    # # [작업2] 이전 일자 ETF 포트폴리오
-    # print('[작업2] 이전 일자 ETF 포트폴리오')
-    # update_krx_etf_data(old_date, 'old_data', engine)
+    # [작업2] 이전 일자 ETF 포트폴리오
+    print('[작업2] 이전 일자 ETF 포트폴리오')
+    update_krx_etf_data(old_date, 'old_data', engine)
 
-    # # [작업3] 최근 일자 ETF 포트폴리오
-    # print('[작업3] 최근 일자 ETF 포트폴리오')
-    # update_krx_etf_data(new_date, 'new_data', engine)
+    # [작업3] 최근 일자 ETF 포트폴리오
+    print('[작업3] 최근 일자 ETF 포트폴리오')
+    update_krx_etf_data(new_date, 'new_data', engine)
 
     # [작업4] 증권사 종목 리포트 업데이트
     print('[작업4] 증권사 종목 리포트 업데이트')
     research = update_research(engine)
 
-    # # # [작업5] ETF 재무제표 작성
-    # print('[작업5] ETF 재무제표 작성')
-    # update_etf_finance(engine)
+    # # [작업5] ETF 재무제표 작성
+    print('[작업5] ETF 재무제표 작성')
+    update_etf_finance(engine)
 
     # [작업6] 목표가 계산
     print('[작업6] 목표가 계산')
@@ -43,7 +43,7 @@ if __name__ == '__main__' :
     create_etf_report_table(research, engine)
     create_etf_base_table(engine)
 
-    # # [작업8] ETF 유사도
-    # print('[작업8] ETF 유사도')
-    # get_etf_similarity(engine)
+    # [작업8] ETF 유사도
+    print('[작업8] ETF 유사도')
+    get_etf_similarity(engine)
 
